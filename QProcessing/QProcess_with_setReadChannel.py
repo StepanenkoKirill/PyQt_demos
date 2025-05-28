c96c8cc39cac2e8ac56c979809ca7060748a46f4
StepanenkoKirill <123152402+StepanenkoKirill@users.noreply.github.com>
2025-05-28 19:53:14 +0300

    Here we use function readAll instead readAllStandardOutput etc. "

---
+++ QProcessing/QProcess_with_setReadChannel.py
@@ -0,0 +1,87 @@
+from PyQt5.QtCore import QProcess, QTextCodec, QCoreApplication
+from PyQt5.QtWidgets import *
+import sys, re
+
+re_pattern = re.compile("[a-zA-Z]+:\s(\d+)%")
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.p = None
+        self.text = QPlainTextEdit()
+        self.text.setReadOnly(True)
+        self.prog_bar = QProgressBar()
+        self.prog_bar.setRange(0, 100)
+        self.btn_start = QPushButton("&Execute")
+        self.btn_start.pressed.connect(self.start_process)
+
+        self.widget = QWidget()
+        self.vLayout = QVBoxLayout()
+        self.vLayout.addWidget(self.prog_bar)
+        self.vLayout.addWidget(self.text)
+        self.vLayout.addWidget(self.btn_start)
+        self.widget.setLayout(self.vLayout)
+        self.setCentralWidget(self.widget)
+
+    def start_process(self):
+        if self.p is None:
+            self.p = QProcess()
+            self.p.readyReadStandardError.connect(self.stderr_handler)
+            self.p.readyReadStandardOutput.connect(self.stdout_handler)
+            self.p.stateChanged.connect(self.state_handler)
+            self.p.finished.connect(self.process_finish_handler)
+            self.p.errorOccurred.connect(self.error_handler)
+            dirr = QCoreApplication.applicationFilePath()
+            self.message(dirr)
+            self.p.start(dirr, ["External.py", "Hello from master"])
+
+    def message(self, text):
+        self.text.appendPlainText(text)
+
+    def stderr_handler(self):
+
+        self.p.setReadChannel(QProcess.ProcessChannel.StandardError)
+        codec = QTextCodec.codecForName("utf8")
+        s = self.p.readAll()
+
+        err = codec.toUnicode(s)
+        st = re_pattern.search(err)
+        if st:
+            number = st.group(1)
+            self.prog_bar.setValue(int(number))
+        else:
+            self.message("There's no appropriate string")
+
+    def stdout_handler(self):
+        self.p.setReadChannel(QProcess.ProcessChannel.StandardOutput)
+        s = self.p.readAll()
+        codec = QTextCodec.codecForName("utf8")
+        out = codec.toUnicode(s)
+        self.message(out)
+
+    def state_handler(self, state):
+        states = {
+            QProcess.ProcessState.NotRunning: "Not Running",
+            QProcess.ProcessState.Starting: "Starting",
+            QProcess.ProcessState.Running: "Running"
+        }
+        state_to_show = states[state]
+        self.message(state_to_show)
+
+    def error_handler(self, error):
+        errors = {
+            QProcess.ProcessError.ReadError: "ReadError",
+            QProcess.ProcessError.WriteError: "WriteError",
+            QProcess.ProcessError.UnknownError: "UnknownError"
+        }
+        error_to_show = errors[error]
+        self.message(error_to_show)
+
+    def process_finish_handler(self):
+        self.message("Process finished")
+        self.p = None
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
